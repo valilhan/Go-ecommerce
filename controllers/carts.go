578eb79da5fa8c711751a184e44f9ed31d317e4b
valihan <ilyasovvalihan@gmail.com>
2023-01-13 16:59:12 +0300

    add database cart

--- controllers/carts.go
+++ controllers/carts.go
@@ -1,24 +1,56 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
 
-func AddProduct() gin.HandlerFunc {
-	
+	"github.com/gin-gonic/gin"
+	"github.com/valilhan/go-ecommerce/database"
+	"github.com/valilhan/go-ecommerce/models"
+)
+
+type EnvCart struct {
+	userModel    models.UserModel
+	ProductModel models.ProductModel
 }
 
-func RemoveItem() gin.HandlerFunc {
-
+func (env *EnvCart) AddProduct() gin.HandlerFunc {
+	//http://localhost:8000/addtocart?id=xxxproduct_idxxx&userID=xxxxxxuser_idxxxxxx
+	return func(c *gin.Context) {
+		productId := c.Query("id")
+		if productId == "" {
+			c.AbortWithError(http.StatusBadRequest, errors.New("Product id is empty"))
+			return
+		}
+		userID := c.Query("userID")
+		if userID == "" {
+			c.AbortWithError(http.StatusBadRequest, errors.New("User id is empty"))
+			return
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		err := database.AddProductToCart(ctx, env.userModel, env.ProductModel, productId, userID)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+		c.IndentedJSON(200, "Successfully add to the cart")
+	}
 }
 
+// func RemoveItem() gin.HandlerFunc {
 
-func GetItemFromCart() gin.HandlerFunc {
+// }
 
-}
+// func GetItemFromCart() gin.HandlerFunc {
 
-func BuyFromCart() gin.HandlerFunc {
+// }
 
-}
+// func BuyFromCart() gin.HandlerFunc {
 
-func InstantBuy() gin.HandlerFunc {
-	
-}
+// }
+
+// func InstantBuy() gin.HandlerFunc {
+
+// }
---
+++ database/cart.go
@@ -0,0 +1,12 @@
+package database
+
+import (
+	"context"
+
+	"github.com/valilhan/go-ecommerce/models"
+)
+
+func AddProductToCart(ctx context.Context, user models.UserModel, product models.ProductModel, productQueryId string, userQueryId string) error {
+
+	return nil
+}
